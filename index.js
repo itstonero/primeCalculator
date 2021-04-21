836fa1299976f992a5b76fe96b1a0fe8faf51f63
Asadu Anthony <asadu.benedict@gmail.com>
2021-04-21 12:06:55 +0100

    Completed Prime Routing

--- index.js
+++ index.js
@@ -3,21 +3,13 @@ require('dotenv').config();
 
 const app = express();
 const hbs = require('express-handlebars');
-const primeController = require('./controller/primeController');
 
 app.set('view engine', 'handlebars');
 app.engine('handlebars', hbs({ layoutsDir: __dirname + '/views/layouts',}));
 
-app.get('/', (req, res) => {
-    const bucketSize = req.query.primeNumbers || 10;
-    if(isNaN(bucketSize))
-    {
-        return res.status(400).json({ message: "primeNumbers must be a number"})
-    }
+app.get('/', require("./routes/primeRouter"));
 
-    const primesMultiples = primeController.generatePrimeMultiples(bucketSize * 1);
-    return res.render('main', {layout : 'index', data:primesMultiples});
-});
 
-app.listen(process.env.PORT, () => console.log(`Prime Multiplier Started At ${process.env.PORT}`));
+const appPort = process.env.PORT || 4000;
+app.listen(appPort, () => console.log(`Prime Multiplier Started At ${appPort}`));
 
---
+++ routes/primeRouter.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const primeController = require('../controller/primeController');
+
+router.get('/', (request, response) => {
+    try{
+        const bucketSize = request.query.primeNumbers || 10;
+        if(isNaN(bucketSize))
+        {
+            return response.status(400).json({ message: "primeNumbers must be a number"})
+        }
+
+        const primesMultiples = primeController.generatePrimeMultiples(bucketSize * 1);
+        return response.render('main', { layout : 'index', data : primesMultiples });
+    }catch(error){
+        console.log(error)
+        return response.status(400).json({ message: 'Something went wrong'})
+    }
+})
+
+module.exports = router;
